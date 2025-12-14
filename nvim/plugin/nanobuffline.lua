364031fdc3aa736948a132089d188212e45b29b7
kausikk <kausikkris10@gmail.com>
2025-12-14 14:27:35 -0800

    Refactor nbl

--- nvim/plugin/nanobuffline.lua
+++ nvim/plugin/nanobuffline.lua
@@ -3,14 +3,16 @@ if _G.nbl_loaded then
 end
 _G.nbl_loaded = true
 
-local M = { buf = -1, buffers = {}, names = {}, columns = {}, lastmark = -1 }
+nbl_buf = -1
+buffers = {}
+names = {}
+columns = {}
+lastmark = -1
+ns_id = vim.api.nvim_create_namespace("_nbl_")
 
-NBL_ROW = 1
-NS_ID = vim.api.nvim_create_namespace("_nbl_")
-
-M.change_buffer = function(ev)
+_change_buffer = function(ev)
 	local ibuf = 0 
-	for i, buf in ipairs(M.buffers) do
+	for i, buf in ipairs(buffers) do
 		if buf == ev.buf then
 			ibuf = i
 			break
@@ -19,21 +21,22 @@ M.change_buffer = function(ev)
 	if ibuf == 0 then
 		return
 	end
-	vim.api.nvim_buf_del_extmark(M.buf, NS_ID, M.lastmark)
-	local col = M.columns[ibuf] - #M.names[ibuf] + 2
-	M.lastmark = vim.api.nvim_buf_set_extmark(
-		M.buf, NS_ID, 0, col, { end_col = M.columns[ibuf], hl_group = "Error"
-	})
-	for _, win in ipairs(vim.fn.win_findbuf(M.buf)) do
-		vim.api.nvim_win_set_cursor(win, { NBL_ROW, col })
+	vim.api.nvim_buf_del_extmark(nbl_buf, ns_id, lastmark)
+	local col = columns[ibuf] - #names[ibuf] + 2
+	lastmark = vim.api.nvim_buf_set_extmark(
+		nbl_buf, ns_id, 0, col, { end_col = columns[ibuf], hl_group = "Error" }
+	)
+	local row = 1
+	for _, win in ipairs(vim.fn.win_findbuf(nbl_buf)) do
+		vim.api.nvim_win_set_cursor(win, { row, col })
 	end
 end
 
-M.list_buffer = function(ev)
+_list_buffer = function(ev)
 	if vim.fn.buflisted(ev.buf) ~= 1 then
 		return
 	end
-	M.window()
+	_window()
 	local name = vim.api.nvim_buf_get_name(ev.buf)
 	if name == "" then
 		name = "  no name"
@@ -41,23 +44,23 @@ M.list_buffer = function(ev)
 		name = "  " .. vim.fn.fnamemodify(name, ":t")
 	end
 	local last = 0
-	if #M.columns > 0 then
-		last = M.columns[#M.columns]
+	if #columns > 0 then
+		last = columns[#columns]
 	end
-	table.insert(M.buffers, ev.buf)
-	table.insert(M.names, name)
-	table.insert(M.columns, last + #name)
-	vim.api.nvim_buf_set_lines(M.buf, 0, 1, false, { table.concat(M.names, "") })
-	vim.bo[M.buf].modified = false
+	table.insert(buffers, ev.buf)
+	table.insert(names, name)
+	table.insert(columns, last + #name)
+	vim.api.nvim_buf_set_lines(nbl_buf, 0, 1, false, { table.concat(names, "") })
+	vim.bo[nbl_buf].modified = false
 end
 
-M.unlist_buffer = function(ev)
+_unlist_buffer = function(ev)
 	if vim.fn.buflisted(ev.buf) ~= 1 then
 		return
 	end
-	M.window()
+	_window()
 	local ibuf = 0 
-	for i, buf in ipairs(M.buffers) do
+	for i, buf in ipairs(buffers) do
 		if buf == ev.buf then
 			ibuf = i
 			break
@@ -66,28 +69,28 @@ M.unlist_buffer = function(ev)
 	if ibuf == 0 then
 		return
 	end
-	local shift = #M.names[ibuf]
-	for i = ibuf + 1, #M.columns do
-		M.columns[i] = M.columns[i] - shift
+	local shift = #names[ibuf]
+	for i = ibuf + 1, #columns do
+		columns[i] = columns[i] - shift
 	end
-	table.remove(M.buffers, ibuf)
-	table.remove(M.names, ibuf)
-	table.remove(M.columns, ibuf)
-	vim.api.nvim_buf_set_lines(M.buf, 0, 1, false, { table.concat(M.names, "") })
-	vim.bo[M.buf].modified = false
+	table.remove(buffers, ibuf)
+	table.remove(names, ibuf)
+	table.remove(columns, ibuf)
+	vim.api.nvim_buf_set_lines(nbl_buf, 0, 1, false, { table.concat(names, "") })
+	vim.bo[nbl_buf].modified = false
 end
 
-M.window = function()
-	if vim.fn.bufwinid(M.buf) ~= -1 then
+_window = function()
+	if vim.fn.bufwinid(nbl_buf) ~= -1 then
 		return
 	end
-	if not vim.api.nvim_buf_is_valid(M.buf) then
-		M.buf = vim.api.nvim_create_buf(false, true)
-		vim.api.nvim_buf_set_name(M.buf, "_nbl_")
-		vim.bo[M.buf].readonly = true
+	if not vim.api.nvim_buf_is_valid(nbl_buf) then
+		nbl_buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_name(nbl_buf, "_nbl_")
+		vim.bo[nbl_buf].readonly = true
 	end
 	-- This fails when doing :%bd or :%bw, idk why, so wrap in pcall
-	local res, win = pcall(vim.api.nvim_open_win, M.buf, false, {
+	local res, win = pcall(vim.api.nvim_open_win, nbl_buf, false, {
 		row = 0, col = 0, relative = "laststatus", height = 1,
 		width = vim.o.columns - 20, style = "minimal"
 	})
@@ -98,8 +101,8 @@ M.window = function()
 	vim.wo[win].winfixbuf = true
 end
 
-vim.api.nvim_create_autocmd("BufEnter", { callback = M.change_buffer })
-vim.api.nvim_create_autocmd("BufAdd", { callback = M.list_buffer })
-vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, { callback = M.unlist_buffer })
+vim.api.nvim_create_autocmd("BufEnter", { callback = _change_buffer })
+vim.api.nvim_create_autocmd("BufAdd", { callback = _list_buffer })
+vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, { callback = _unlist_buffer })
 
-M.window()
+_window()
